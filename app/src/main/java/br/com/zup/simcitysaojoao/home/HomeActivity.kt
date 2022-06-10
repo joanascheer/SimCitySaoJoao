a9e532946268f79db9efa1a62ad1d126d71e6258
joanascheer <scheer.joana@gmail.com>
2022-06-10 11:58:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/zup/simcitysaojoao/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.simcitysaojoao.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.zup.simcitysaojoao.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
