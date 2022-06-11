6b52d1c61641e17c1c48338f462997a411d27c41
joanascheer <scheer.joana@gmail.com>
2022-06-11 16:11:21 -0300

    Mostrando detalhes do item clicado concluído

--- app/src/main/java/br/com/zup/simcitysaojoao/produtos/DetalhesDeProdutosFragment.kt
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/DetalhesDeProdutosFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import br.com.zup.simcitysaojoao.PRODUCT_KEY
 import br.com.zup.simcitysaojoao.databinding.FragmentDetalhesDeProdutosBinding
+import br.com.zup.simcitysaojoao.model.Produto
 
 class DetalhesDeProdutosFragment : Fragment() {
     private lateinit var binding: FragmentDetalhesDeProdutosBinding
@@ -19,6 +21,17 @@ class DetalhesDeProdutosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recuperarDadosProduto()
+    }
+
+    private fun recuperarDadosProduto() {
+        val produto = arguments?.getParcelable<Produto>(PRODUCT_KEY)
+        if (produto != null) {
+            binding.tvNomeProdutoTelaDetalhes.text = produto.getNome()
+            binding.tvQuantidadeProdutoTelaDetalhes.text = produto.getQuantidade().toString()
+            binding.tvValorProdutoTelaDetalhes.text = produto.getValorUnitario().toString()
+            binding.tvReceitaProdutoTelaDetalhes.text = produto.getReceita()
+        }
     }
 
 }
